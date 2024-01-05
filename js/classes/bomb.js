551c2f8515ead0770d27c6cedc3f9f6f48759410
hmahar <harrimahar@gmail.com>
2024-01-05 14:15:33 +0200

    MYG-16 Added explosions

--- js/classes/bomb.js
+++ js/classes/bomb.js
@@ -1,6 +1,7 @@
-import { TILESIZE, BOMBSPEED } from "../config.js"
+import { TILESIZE, BOMBSPEED, SPRITES } from "../config.js"
 import { map, entities } from "../maps/map.js";
 import { collisionMapRefresh } from "../helpers/collisionDetection.js";
+import { animate, stopAnimate } from "../script.js";
 
 export class Bomb{
     constructor(x, y, id, htmlElem, position) {
@@ -9,7 +10,7 @@ export class Bomb{
         this.id = id;
         this.htmlElem = htmlElem;
         this.position = position;
-        this.power = 2;
+        this.power = 3;
         //Place bomb to the field
         htmlElem.style.left = x * TILESIZE + "px";
         htmlElem.style.top = y * TILESIZE + "px";
@@ -33,25 +34,25 @@ export class Bomb{
 
       //TEST
     explode() {
-        this.htmlElem.className = "explode"
+        this.htmlElem.className = "space"
 
-        setTimeout(() => {
-            this.htmlElem.style.backgroundPosition = "-64px -352px"
-        }, 100)
+        // setTimeout(() => {
+        //     this.htmlElem.style.backgroundPosition = "-64px -352px"
+        // }, 100)
 
-        setTimeout(() => {
-            this.htmlElem.style.backgroundPosition = "-224px -192px"
-        }, 200)
+        // setTimeout(() => {
+        //     this.htmlElem.style.backgroundPosition = "-224px -192px"
+        // }, 200)
 
-        setTimeout(() => {
-            this.htmlElem.style.backgroundPosition = "-224px -352px"
-        }, 300)
+        // setTimeout(() => {
+        //     this.htmlElem.style.backgroundPosition = "-224px -352px"
+        // }, 300)
 
-        setTimeout(() => {
-            this.htmlElem.className = "space"
-        }, 400)
+        // setTimeout(() => {
+        //     this.htmlElem.className = "space"
+        // }, 400)
         
-        let explosionArray = [{x: this.x, y: this.y}];
+        let explosionArray = [{x: this.x, y: this.y, exPower: 0, piece: 'center', isLast: false}];
         let breakArray = [];
         let down, right, up, left = false;
         //calculates bomb directions and how far the bomb explodes
@@ -59,7 +60,11 @@ export class Bomb{
           //check if index goes out of range
 
           if(this.y + i < map.length && map[this.y+i][this.x] == 0 && !down){
-            explosionArray.push({x: this.x, y: this.y+i})
+            if(i == this.power){
+              explosionArray.push({x: this.x, y: this.y+i, exPower: i, piece: 'down', isLast: true})
+            }else{
+              explosionArray.push({x: this.x, y: this.y+i, exPower: i, piece: 'down'})
+            }
             //breaking logic
           }else if (this.y + i < map.length && map[this.y+i][this.x] == 2 && !down){
             breakArray.push({x: this.x, y: this.y+i})
@@ -68,7 +73,12 @@ export class Bomb{
             down = true;
           }
           if(this.x + i < map[0].length && map[this.y][this.x+i] == 0 && !right){
-            explosionArray.push({x: this.x+i, y: this.y})
+            if(i == this.power){
+              explosionArray.push({x: this.x+i, y: this.y, exPower: i, piece: 'right', isLast: true})
+            }else{
+              explosionArray.push({x: this.x+i, y: this.y, exPower: i, piece: 'right'})
+
+            }
           }else if (this.x + i < map[0].length && map[this.y][this.x+i] == 2 && !right){
             breakArray.push({x: this.x+i, y: this.y})
             right = true;
@@ -76,7 +86,12 @@ export class Bomb{
             right = true;
           }
           if(this.y - i >= 0 && map[this.y-i][this.x] == 0 && !up){
-            explosionArray.push({x: this.x, y: this.y-i})
+            if(i == this.power){
+              explosionArray.push({x: this.x, y: this.y-i, exPower: i, piece: 'up', isLast: true})
+            }else{
+              explosionArray.push({x: this.x, y: this.y-i, exPower: i, piece: 'up'})
+
+            }
           }else if (this.y - i >= 0 && map[this.y-i][this.x] == 2 && !up){
             breakArray.push({x: this.x, y: this.y-i})
             up = true;
@@ -84,7 +99,13 @@ export class Bomb{
             up = true;
           }
           if(this.x - i >= 0 && map[this.y][this.x-i] == 0 && !left){
-            explosionArray.push({x: this.x-i, y: this.y})
+            if(i == this.power){
+              explosionArray.push({x: this.x-i, y: this.y, exPower: i, piece: 'left', isLast: true})
+
+            }else{
+              explosionArray.push({x: this.x-i, y: this.y, exPower: i, piece: 'left'})
+
+            }
           }else if (this.x - i >= 0 && map[this.y][this.x-i] == 2 && !left){
             breakArray.push({x: this.x-i, y: this.y})
             left = true;
@@ -92,10 +113,27 @@ export class Bomb{
             left = true;
           }
         }
+        console.log(explosionArray);
+
+        //Check which block is which vertical/horisontal/end piece
+        for(let item of explosionArray){
+          if(!item.rotation){
+            const pieceCountWithEnd = explosionArray.reduce((count, obj) => count + (obj.piece === item.piece ? 1 : 0), 0);
+            const pieceCountWithOutEnd = explosionArray.reduce((count, obj) => count + (obj.piece === item.piece && obj.isLast === undefined ? 1 : 0), 0);
+            if(pieceCountWithEnd == pieceCountWithOutEnd){
+              item['isLast'] = true;
+            }else if(item.isLast === undefined){
+              item['isLast'] = false;
+            }
+          }
+        }
+
 
         //explosion animation
         for(let item of explosionArray){
           let tile = document.getElementsByClassName(`${item.y}-${item.x}`);
+          //animate bomb
+          let bombExplosionId = animate(tile[0])
           tile[0].style.backgroundPosition = "-64px -352px";
           //check for player / enemy collision with bomb
           for(let entity of entities){
@@ -107,8 +145,14 @@ export class Bomb{
         //block breaking logic
         for(let item of breakArray){
           let tile = document.getElementsByClassName(`${item.y}-${item.x}`);
-          tile[0].className = `tile space ${item.y}-${item.x}`
+          //animate breaking block
+          let breakBlockAnimationId = animate(tile[0], SPRITES.breakableWall.startPosX, SPRITES.breakableWall.endPosX, SPRITES.breakableWall.Y, 400);
           map[item.y][item.x] = 0;
+          setTimeout(() => {
+            stopAnimate(breakBlockAnimationId);
+            tile[0].className = `tile space ${item.y}-${item.x}`
+            tile[0].style.backgroundPosition = ''
+          }, 400);
         }
         collisionMapRefresh(map);
   }
